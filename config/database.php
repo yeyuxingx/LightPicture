22848a564af29be0593fead701d10f7857a21cb1
yeyuxingx <105349069+yeyuxingx@users.noreply.github.com>
2023-09-17 15:48:49 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -24,13 +24,13 @@ return [
             // 数据库类型
             'type'            => env('database.type', 'mysql'),
             // 服务器地址
-            'hostname'        => env('database.hostname', '127.0.0.1'),
+            'hostname'        => env('database.hostname', 'mysql.sqlpub.com'),
             // 数据库名
-            'database'        => env('database.database', ''),
+            'database'        => env('database.database', 'imglxloveltop'),
             // 用户名
-            'username'        => env('database.username', 'root'),
+            'username'        => env('database.username', 'yeyu2168267227'),
             // 密码
-            'password'        => env('database.password', ''),
+            'password'        => env('database.password', 'ef2a412ae1ade883'),
             // 端口
             'hostport'        => env('database.hostport', '3306'),
             // 数据库连接参数
